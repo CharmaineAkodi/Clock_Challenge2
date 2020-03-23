b09674623faa9b726f91f8f7a4a6676748272115
CharmaineAkodi <51997303+CharmaineAkodi@users.noreply.github.com>
2020-03-23 19:10:14 +0100

    verbetering in standaard code
    tijd en datum update

--- scripts/main.js
+++ scripts/main.js
@@ -1,88 +1,46 @@
 //TIME
-var today = new Date();
+function showTime(){
+	var time = new Date();
+	var hours = time.getHours();
+	var minutes = time.getMinutes();
+	var seconds = time.getSeconds();
 
-document.getElementById('hours').innerHTML = today.getHours();
-document.getElementById('minutes').innerHTML = today.getMinutes();
+	//add 0 from function (don't move)
+	hours = addZero(hours);
+	minutes = addZero(minutes);
+	seconds = addZero(seconds);
+
+	document.getElementById('time').innerHTML = hours + ":" + minutes + ":" + seconds;
+
+	setInterval(showTime, 1000);
+
+	//add 0 before hour/minute/seconds < 10
+	function addZero(i){
+		if(i < 10){
+			return i = "0" + i;
+		}else{
+			return i;
+		}
+	}
+}
+showTime();
 
 //DATE
-var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
-document.getElementById('date').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()];
+var date = new Date();
+var months = ['Januari', 'Februari','Maart','April','Mei','Juni','Juli','Augustus','September','Oktober','November','December'];
+var showDate = date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
+
+document.getElementById('date').innerHTML = showDate;
+
+//GREETING ACCORDING TO TIME
+function greeting(){
+	
+}
+
+
+//TIMEZONE
+
 
 // //ICON & BACKGROUND CHANGE
-// function changeTheme(){
-// 	if (hours >= 7 && < 12) {
-
-// 	}
-// 	if (hours >= 12 && < 18) {
-
-// 	}
-// 	if (hours >= 18 && < 0) {
-
-// 	}
-// 	if (hours >= 0 && < 7) {
-
-// 	}
-// };
-
-// function greeting(){
-// 	if (hours = 7) {
-// 	//GOODMORNING
-// 	}
-// 	if (hours = 22) {
-// 	//GOODNIGHT
-// 	}
-// }
-// // TIME
-// function initClock() {
-// 	renderClock();
-// 	setInterval(renderClock, 1000);
-// }
-
-// function renderClock() {
-// 	var clock = document.getElementById('clock');
-// 	var time  = getCurrentTime(new Date());
-// 	var sep   = flashSeperator(time['seconds']);
-
-// 	clock.innerHTML = time['hours'] + sep +  time['minutes'];
-// }
-
-// function flashSeperator(seconds) {
-// 	var sepClass = '';
-
-// 	if (seconds % 2 === 1) {
-// 		sepClass = ' class="trans"';
-// 	}
-// 	return '<span' + sepClass + '">:</span>';
-// }
-
-// function getCurrentTime(date) {
-// 	var time = [];
-
-// 	time['seconds'] = date.getSeconds();
-// 	time['minutes'] = date.getMinutes(),
-// 	time['hours']   = date.getHours();
-
-// 	if (time['hours'] < 10) {
-// 		time['hours'] = '0' + time['hours'];
-// 	}
-// 	if (time['minutes'] < 10) {
-// 		time['minutes'] = '0' + time['minutes'];
-// 	}
-// 	return time;
-// }
-
-// initClock();
-
-// //DATE
 
 
-
-// //
-// new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
-
-
-// //BACKGROUND CHANGE
-// borderBtn.addEventListener('click', setMorningColor);
-// bgColorBtn.addEventListener('click', setDayColor);
-// colorBtn.addEventListener('click', setEveningColor);
-// colorBtn.addEventListener('click', setNightColor);
